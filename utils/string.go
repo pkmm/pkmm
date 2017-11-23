8af670530e92124cc24c78de6297e650b766d28a
robotgg <robotgg@126.com>
2017-11-23 20:17:36 +0800

    en

--- utils/string.go
+++ utils/string.go
@@ -3,10 +3,16 @@ package utils
 import (
 	"crypto/md5"
 	"fmt"
+	"time"
 )
 
 func Md5(buf []byte) string {
 	hash := md5.New()
 	hash.Write(buf)
 	return fmt.Sprintf("%x", hash.Sum(nil))
+}
+
+func Today() int{
+	loc, _ := time.LoadLocation("Asia/Shanghai")
+	return time.Now().In(loc).Day()
 }
