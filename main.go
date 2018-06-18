eaccd23e8a0ba22fa6ee9ed72eeefe1115017405
pkmm <robotgg@126.com>
2018-06-18 17:58:14 +0800

    Move router in main to router.go

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/astaxie/beego"
 	_ "github.com/go-sql-driver/mysql"
-	"pkmm/controllers"
 	_ "pkmm/database/migrations"
 	"pkmm/models"
 	_ "pkmm/routers"
@@ -19,11 +18,6 @@ func main() {
 
 	// 数据库迁移
 	//migration.Upgrade(0)
-	beego.Router("/", &controllers.ForumController{}, "*:GetForums")
-	beego.Router("/zf", &controllers.ZfController{}, "*:GetScore")
-	beego.Router("/zf/check_account", &controllers.ZfController{}, "*:CheckAccount")
-	beego.Router("/zf/update_account", &controllers.ZfController{}, "post:UpdateAccount")
-	beego.Router("/zf/get_failed_lessons", &controllers.ZfController{}, "*:GetFailedLessons")
 
 	// 设置日志
 	beego.SetLogger("file", `{"filename":"logs/pkmm.log","level":7,"daily":true,"maxdays":2}`)
--- routers/router.go
+++ routers/router.go
@@ -30,5 +30,15 @@ func init() {
 	)
 	beego.AddNamespace(ns)
 
+	// 指定路由
+	beego.Router("/", &controllers.ForumController{}, "*:GetForums")
+	beego.Router("/zf", &controllers.ZfController{}, "*:GetScore")
+	beego.Router("/zf/check_account", &controllers.ZfController{}, "*:CheckAccount")
+	beego.Router("/zf/update_account", &controllers.ZfController{}, "post:UpdateAccount")
+	beego.Router("/zf/get_failed_lessons", &controllers.ZfController{}, "*:GetFailedLessons")
+
+	// 自动路由
 	beego.AutoRouter(&controllers.WXController{})
+
+
 }
