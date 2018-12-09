e684299b15adf04a1c1621ee6ceec5163dc86653
pkmm <robotgg@126.com>
2018-12-09 16:18:11 +0800

    添加server 酱推送

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
 	_ "github.com/go-sql-driver/mysql"
+	"net/http"
+	"net/url"
 	_ "pkmm/database/migrations"
 	"pkmm/models"
 	_ "pkmm/routers"
 	"pkmm/utils"
-	"fmt"
 	"time"
 )
 
@@ -28,16 +30,32 @@ func main() {
 	// 部署Email提醒
 	runMode := beego.AppConfig.String("runmode")
 	if (runMode != "dev") {
-		utils.SendMail(
-			"xiaoccla@qq.com",
-			"xiaoccla",
-			"部署HOOK通知",
-			fmt.Sprintf(
-				"pkmm代码已经重新部署, Time: [%s], IP: [%s]",
-				beego.Date(time.Now(), "Y-m-d H:i:s"),
-				utils.IpAddressOfLocal(),
-			),
-			[]string{},
+		// utils.SendMail(
+		// 	"xiaoccla@qq.com",
+		// 	"xiaoccla",
+		// 	"部署HOOK通知",
+		// 	fmt.Sprintf(
+		// 		"pkmm代码已经重新部署, Time: [%s], IP: [%s]",
+		// 		beego.Date(time.Now(), "Y-m-d H:i:s"),
+		// 		utils.IpAddressOfLocal(),
+		// 	),
+		// 	[]string{},
+		// )
+
+		// 使用server酱推送
+		client := &http.Client{}
+		desp := fmt.Sprintf(
+			"pkmm代码已经重新部署, Time: [%s], IP: [%s]",
+			beego.Date(time.Now(), "Y-m-d H:i:s"),
+			utils.IpAddressOfLocal(),
+		)
+		data := url.Values{
+			"text": {"Pkmm Server 代码发生部署"},
+			"desp": {desp},
+		}
+		client.PostForm(
+			"https://sc.ftqq.com/SCU37342T301edeeefbdc09a20b7476e379f84a065c0cb87c9592c.send",
+			data,
 		)
 	}
 
